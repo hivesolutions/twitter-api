6ca999c48e4acf6004d0bae4c7b1da9fd1372d07
joamag <joamag@gmail.com>
2016-03-14 23:55:00 +0000

    multiple kwargs in apps

--- src/examples/app.py
+++ src/examples/app.py
@@ -43,8 +43,12 @@ from . import base
 
 class TwitterApp(appier.WebApp):
 
-    def __init__(self):
-        appier.WebApp.__init__(self, name = "twitter")
+    def __init__(self, *args, **kwargs):
+        appier.WebApp.__init__(
+            self,
+            name = "twitter",
+            *args, **kwargs
+        )
 
     @appier.route("/", "GET")
     def index(self):
