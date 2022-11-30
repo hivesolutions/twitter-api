d6dd47dbced05c365703d2dcfef9faa041dbde17
João Magalhães <joamag@gmail.com>
2022-11-30 16:45:03 +0000

    feat: renaming of repo

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ import os
 import setuptools
 
 setuptools.setup(
-    name = "twitter_api",
+    name = "twitter-api",
     version = "0.1.7",
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
